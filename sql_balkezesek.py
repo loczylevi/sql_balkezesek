f205af0390e91a79287d5f050653cf5fbd072e6b
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2022-12-13 13:26:54 +0100

    Update sql_balkezesek.py

--- sql_balkezesek.py
+++ sql_balkezesek.py
@@ -84,32 +84,3 @@ print(f"6.feladat: {eredmeny} font")
 
 
 
-
-"""
-# 3. Hány sornyi adat van a forrásállományban?
-
-darab = sql(c, " SELECT COUNT() FROM tb  ")[0][0]     
-print( f'3. feladat: { darab } ' )
-
-# 4. Fürge Ferenc a Gran Prix Circuit pályán futott 3. körét hány másodperc alatt teljesítette?
-
-másodperc = sql(c, " SELECT sec FROM tb WHERE versenyzo == 'Fürge Ferenc' AND palya == 'Gran Prix Circuit' AND kor == 3 ")[0][0]
-print( f'4. feladat: { másodperc } másodperc ' )
-
-#5. Kérjen be egy nevet.
-
-print( f'5. feladat:      ' )
-print( f'Kérem egy versenyző nevét:')
-nev = input()
-
-# 6. A {nev} versenyző hol futotta a leggyorsabb körét?
-
-palya, korido = sql(c, " SELECT palya, MIN(korido) FROM tb WHERE versenyzo == ? ", (nev,) )[0]
-
-if palya == None:
-    print( f'6. feladat: Nincs ilyen versenyzo az állományban' )
-else:    
-    print( f'6. feladat: { palya },  {korido} ' )
-
-"""
-
